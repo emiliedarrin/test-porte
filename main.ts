c3a5428c9b7be4f10828843dc4117e4981c7d311
emiliedarrin <emilie.darrin@gmail.com>
2026-05-02 15:18:48 -0400

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
 basic.forever(function () {
-	
+    if (sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P0) <= 8) {
+        basic.showIcon(IconNames.Yes)
+        wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 320)
+        basic.pause(2000)
+        wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 0)
+    } else if (sonarbit.sonarbit_distance(Distance_Unit.Distance_Unit_cm, DigitalPin.P0) > 8) {
+        basic.showIcon(IconNames.No)
+        wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 0)
+    }
 })
